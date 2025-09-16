4378b25a942612febefecc45a1e0025510d17ab8
Полина Пенькова <fishofmydream010905@bk.ru>
2025-09-16 18:06:59 +0700

    информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,29 @@ from flask import Flask
 app = Flask (__name__)
 
 @app.route ("/")
-def start():
+@app.route ("/web")
+def web():
     return """<!doctype html>
         <html>
             <body>
                <h1>web-сервер на flask</h1> 
+               <a href="/author">author</a>
            </body>
         </html>"""
+
+@app.route ("/author")
+def author():
+    name = "пенькова полина александровна"
+    group = "ФБИ-34"
+    faculity = "ФБ"
+
+    return """ <!doctype html>
+        <html>
+            <body>
+                <p>студент: """ + name + """</p>
+                <p>группа: """ + group + """</p>
+                <p>факультет: """ + faculity + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
+
