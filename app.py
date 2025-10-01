0065bc29402081bd338534a981b4a670fe530351
Полина Пенькова <fishofmydream010905@bk.ru>
2025-10-01 19:56:41 +0700

    исправление адресов

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def not_found(err):
     return "нет такой страницы((((", 404
 
 @app.route ("/")
-@app.route ("/web")
+@app.route ("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
@@ -19,7 +19,7 @@ def web():
             "X-Server": "sample", 
             'Content-Type': 'text/plain; charset=utf-8'}
 
-@app.route ("/author")
+@app.route ("/lab1/author")
 def author():
     name = "пенькова полина александровна"
     group = "ФБИ-34"
@@ -35,7 +35,7 @@ def author():
             </body>
         </html>"""
 
-@app.route ('/image')
+@app.route ('/lab1/image')
 def image():
     path = url_for("static", filename="oak.jpg")
     css_path = url_for("static", filename="lab1.css")
@@ -59,7 +59,7 @@ def clear_counter():
     count = 0
     return redirect('/counter')
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count 
     count += 1 
@@ -79,7 +79,7 @@ def counter():
     </body>
 </html>
 '''
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
     return redirect("/author")
 
