510e7fc7089d46ab25adeffdd9be1d41a969f0b3
Полина Пенькова <fishofmydream010905@bk.ru>
2025-10-03 23:46:49 +0700

    породы собак

--- app.py
+++ app.py
@@ -375,3 +375,135 @@ def calc(a, b):
                         div_result=div_result,
                         pow_result=pow_result)
 
+
+books = [
+    {'author': 'Джордж Оруэлл', 'title': '1984', 'genre': 'Антиутопия', 'pages': 328},
+    {'author': 'Джоан Роулинг', 'title': 'Гарри Поттер и философский камень', 'genre': 'Фэнтези', 'pages': 432},
+    {'author': 'Джон Р. Р. Толкин', 'title': 'Властелин колец', 'genre': 'Фэнтези', 'pages': 1178},
+    {'author': 'Агата Кристи', 'title': 'Убийство в Восточном экспрессе', 'genre': 'Детектив', 'pages': 256},
+    {'author': 'Стивен Кинг', 'title': 'Оно', 'genre': 'Ужасы', 'pages': 1248},
+    {'author': 'Харпер Ли', 'title': 'Убить пересмешника', 'genre': 'Роман воспитания', 'pages': 416},
+    {'author': 'Фрэнсис Скотт Фицджеральд', 'title': 'Великий Гэтсби', 'genre': 'Классика', 'pages': 256},
+    {'author': 'Джейн Остин', 'title': 'Гордость и предубеждение', 'genre': 'Любовный роман', 'pages': 480},
+    {'author': 'Эрнест Хемингуэй', 'title': 'По ком звонит колокол', 'genre': 'Военная проза', 'pages': 576},
+    {'author': 'Габриэль Гарсия Маркес', 'title': 'Сто лет одиночества', 'genre': 'Магический реализм', 'pages': 544},
+    {'author': 'Пауло Коэльо', 'title': 'Алхимик', 'genre': 'Притча', 'pages': 256},
+    {'author': 'Дэн Браун', 'title': 'Код да Винчи', 'genre': 'Детективный триллер', 'pages': 480},
+    {'author': 'Маргарет Митчелл', 'title': 'Унесенные ветром', 'genre': 'Исторический роман', 'pages': 1024},
+    {'author': 'Артур Конан Дойл', 'title': 'Приключения Шерлока Холмса', 'genre': 'Детектив', 'pages': 384},
+    {'author': 'Оскар Уайльд', 'title': 'Портрет Дориана Грея', 'genre': 'Философский роман', 'pages': 320},
+    {'author': 'Рэй Брэдбери', 'title': '451° по Фаренгейту', 'genre': 'Научная фантастика', 'pages': 256},
+    {'author': 'Станислав Лем', 'title': 'Солярис', 'genre': 'Научная фантастика', 'pages': 288},
+    {'author': 'Умберто Эко', 'title': 'Имя розы', 'genre': 'Исторический детектив', 'pages': 672}
+]
+
+@app.route('/lab2/books/')
+def show_books():
+    return render_template('books.html', books=books)
+
+dogs = [
+    {
+        'name': 'Лабрадор-ретривер',
+        'description': 'Дружелюбная и активная семейная собака',
+        'image': 'labrador.jpg'
+    },
+    {
+        'name': 'Немецкая овчарка',
+        'description': 'Умная и преданная рабочая порода',
+        'image': 'german_shepherd.jpg'
+    },
+    {
+        'name': 'Золотистый ретривер',
+        'description': 'Добродушная и терпеливая собака',
+        'image': 'golden_retriever.jpg'
+    },
+    {
+        'name': 'Французский бульдог',
+        'description': 'Компактная и дружелюбная городская собака',
+        'image': 'french_bulldog.jpg'
+    },
+    {
+        'name': 'Бигль',
+        'description': 'Энергичная и любопытная гончая',
+        'image': 'beagle.jpg'
+    },
+    {
+        'name': 'Пудель',
+        'description': 'Умная и элегантная порода',
+        'image': 'poodle.jpg'
+    },
+    {
+        'name': 'Ротвейлер',
+        'description': 'Сильная и уверенная в себе собака',
+        'image': 'rottweiler.jpg'
+    },
+    {
+        'name': 'Йоркширский терьер',
+        'description': 'Маленькая собака с большим характером',
+        'image': 'yorkie.jpg'
+    },
+    {
+        'name': 'Боксёр',
+        'description': 'Энергичная и игривая порода',
+        'image': 'boxer.jpg'
+    },
+    {
+        'name': 'Такса',
+        'description': 'Смелая и любопытная собака',
+        'image': 'dachshund.jpg'
+    },
+    {
+        'name': 'Сибирский хаски',
+        'description': 'Энергичная ездовая собака',
+        'image': 'husky.jpg'
+    },
+    {
+        'name': 'Доберман',
+        'description': 'Элегантная и умная порода',
+        'image': 'doberman.jpg'
+    },
+    {
+        'name': 'Австралийская овчарка',
+        'description': 'Умная и энергичная рабочая собака',
+        'image': 'australian_shepherd.jpg'
+    },
+    {
+        'name': 'Ши-тцу',
+        'description': 'Нежная и дружелюбная собака-компаньон',
+        'image': 'shih_tzu.jpg'
+    },
+    {
+        'name': 'Бернский зенненхунд',
+        'description': 'Спокойная и добродушная пастушья собака',
+        'image': 'bernese_mountain.jpg'
+    },
+    {
+        'name': 'Померанский шпиц',
+        'description': 'Маленькая и пушистая собака',
+        'image': 'pomeranian.jpg'
+    },
+    {
+        'name': 'Кавалер кинг чарльз спаниель',
+        'description': 'Нежная и ласковая собака',
+        'image': 'cavalier.jpg'
+    },
+    {
+        'name': 'Мопс',
+        'description': 'Добродушная и компактная собака',
+        'image': 'pug.jpg'
+    },
+    {
+        'name': 'Бордер-колли',
+        'description': 'Самая умная порода собак',
+        'image': 'border_collie.jpg'
+    },
+    {
+        'name': 'Чихуахуа',
+        'description': 'Самая маленькая порода собак',
+        'image': 'chihuahua.jpg'
+    }
+]
+
+@app.route('/lab2/dogs/')
+def show_dogs():
+    return render_template('dogs.html', dogs=dogs)
