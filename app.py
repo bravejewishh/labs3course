ac989ca1b6fb57d6683cf72850098e13e713be4e
полина пенькова <fishofmydream010905@bk.ru>
2025-10-04 11:26:09 +0700

    blabla

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, url_for, request, redirect, make_response, abort, rende
 import datetime
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask (__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.errorhandler(500)
 def internal_server_error(err):
@@ -125,6 +127,8 @@ def index():
             <ul>
                 <li><a href="/lab1">Первая лабораторная</a></li>
                 <li><a href="/lab2">Вторая лабораторная</a></li>
+                <li><a href="/lab3">Третья лабораторная</a></li>
+
             </ul>
         </main>
         <footer>
