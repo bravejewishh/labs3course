91dd22991aa830009b0abb605e0cd9d26e703393
Полина Пенькова <fishofmydream010905@bk.ru>
2025-10-17 16:50:52 +0700

    валидация

--- lab3.py
+++ lab3.py
@@ -25,7 +25,12 @@ def del_cookie():
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'заполните это поле'
     age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'заполните это поле'
     sex = request.args.get('sex')
-    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
