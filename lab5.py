83469863913abdcfadd4cebfdce48a1c2409b3b4
полина пенькова <fishofmydream010905@bk.ru>
2025-11-01 09:28:51 +0700

    update

--- lab5.py
+++ lab5.py
@@ -5,18 +5,18 @@ lab5 = Blueprint('lab5', __name__)
 def lab():
     return render_template('lab5/lab5.html')
 
-@lab5.route('lab5/login')
+@lab5.route('/lab5/login')
 def login():
     return "Страница входа"
 
-@lab5.route('lab5/register')
+@lab5.route('/lab5/register')
 def register():
     return "Страница регистрации"
 
-@lab5.route('lab5/list')
+@lab5.route('/lab5/list')
 def list_articles():
     return "Список статей"
 
-@lab5.route('lab5/create')
+@lab5.route('/lab5/create')
 def create_article():
     return "Создать статью"
