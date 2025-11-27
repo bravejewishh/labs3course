6443e40bcafa5dc151afc3b9cd5d43aef1a74e47
Полина Пенькова <fishofmydream010905@bk.ru>
2025-11-28 02:03:28 +0700

    fhf

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, render_template, request, session, redirect, url_for, current_app
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
