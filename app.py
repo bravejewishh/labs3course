848b45ce97dfcf2ae06f30d10220e2bf2acc20f3
Полина Пенькова <fishofmydream010905@bk.ru>
2025-11-28 01:53:31 +0700

    os

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for, request, redirect, make_response, abort, render_template, session, current_app
 import datetime
+import os 
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
