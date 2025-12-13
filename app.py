efca9c0964e662e3a205bf516f904f5bec9e9131
полина пенькова <fishofmydream010905@bk.ru>
2025-12-13 10:42:18 +0700

    модель базы данных

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, url_for, request, redirect, make_response, abort, render_template, session, current_app
 import datetime
 import os 
+from os import path
+
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -8,11 +13,28 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 app = Flask (__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'самый страшный секрет на свете')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'polina_penkova_orm'
+    db_user = 'polina_penkova_orm'
+    db_password = '12345'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, 'polina_penkova_orm.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -21,6 +43,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 @app.errorhandler(500)
 def internal_server_error(err):
